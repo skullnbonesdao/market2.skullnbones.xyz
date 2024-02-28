9e97dcae9620c8acc90930df45aec0d4035dba8a
skullandbonesdao <skullandbonesdao@gmail.com>
2024-02-28 18:11:03 +0100

    fixed issue where offset by 1h

--- src/stores/calc_passed_time.ts
+++ src/stores/calc_passed_time.ts
@@ -1,6 +1,6 @@
 export function calc_passed_time(timestamp_to_get_since: number): string {
   const now = Date.now();
-  const old: any = new Date(timestamp_to_get_since);
+  const old:any = new Date(timestamp_to_get_since + "Z");
   const diffMs = now - old;
   const diffDays = Math.floor(diffMs / 86400000); // days
   const diffHrs = Math.floor((diffMs % 86400000) / 3600000); // hours
